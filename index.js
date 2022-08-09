bf657dd9953cfd72713f2426178c227b8f754739
Lia Mageo <>
2022-08-09 16:15:53 +0000

    add changes to index

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
+var http = require("http"); 
+http.createServer(function(req,res) { // request
+  var path = req.url.toLowerCase(); // turns path to lowercase
+  switch(path) { // allows us to see different outputs
+    case '/': 
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('Home page');
+      break;
+    case '/about': // we see about page
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('About page');
+      break;
+    default:
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end('Not found'); // not found will appear
+      break;
+    }
 }).listen(process.env.PORT || 3000);
